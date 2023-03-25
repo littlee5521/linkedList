9379a9d3c28493b6928ec8af4909ac94fd94a0a3
Martin <68254461+littlee5521@users.noreply.github.com>
2023-03-24 22:02:57 -0700

    methods to append, prepend, grab the tail, find item at index, pop item, and grab index of item have been added

--- linkedList.js
+++ linkedList.js
@@ -1,20 +1,79 @@
 var list = /** @class */ (function () {
     function list(arraytoSort) {
+        //temporary work around because TS was throwing a error about this being possibly undefined
         var temp = arraytoSort[0];
         arraytoSort.shift();
         this.head = new node(temp, cat(arraytoSort));
     }
     list.prototype.length = function (count, item) {
+        // this is meant for the first run sets the head to work from
         if (item == undefined) {
             item = this.head;
             count = 1;
-            console.log("i ran");
         }
+        //this is the base case returns the count when it finds tai;
         if ((item === null || item === void 0 ? void 0 : item.next) == undefined) {
             return count;
         }
         count++;
         return this.length(count, item.next);
+    };
+    list.prototype.append = function (value) {
+        //intializes the temp to be the head, works down froim there looking for tail
+        var temp = this.head;
+        while (temp.next != undefined) {
+            temp = temp.next;
+        }
+        temp.next = new node(value);
+    };
+    list.prototype.prepend = function (value) {
+        // makes a temporary new variable to store the current head and then makes a new node with its value
+        var temp = new node(value, this.head);
+        this.head = temp;
+    };
+    list.prototype.tail = function () {
+        //moves down the list and returns the last item
+        var temp = this.head;
+        while (temp.next != undefined) {
+            temp = temp.next;
+        }
+        return temp;
+    };
+    list.prototype.at = function (index) {
+        var temp = this.head;
+        var count = 0;
+        if (index > this.length()) {
+            return null;
+        }
+        while (count != index && temp.next != undefined) {
+            temp = temp.next;
+            count++;
+        }
+        return temp;
+    };
+    list.prototype.pop = function () {
+        var _a;
+        var temp = this.indexof(this.tail().data);
+        if (temp == undefined) {
+            return null;
+        }
+        var index = temp - 1;
+        var item = this.at(index);
+        var value = (_a = item === null || item === void 0 ? void 0 : item.next) === null || _a === void 0 ? void 0 : _a.data;
+        item.next = undefined;
+        return value;
+    };
+    list.prototype.indexof = function (item) {
+        var temp = this.head;
+        var count = 0;
+        while (item != temp.data) {
+            if (temp.next == undefined) {
+                return null;
+            }
+            temp = temp.next;
+            count++;
+        }
+        return count;
     };
     return list;
 }());
@@ -26,6 +85,7 @@ var node = /** @class */ (function () {
     return node;
 }());
 var cat = function (array) {
+    // this works through the array returnign items as nodes
     if (array.length == 0) {
         return;
     }
@@ -33,6 +93,7 @@ var cat = function (array) {
     array.shift();
     return new node(value, cat(array));
 };
-var test = new list([1, 3, 4]);
-console.log(test.head);
-console.log(test.length());
+var test = new list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(test.tail());
+console.log(test.pop());
+console.log(test.tail());
--- linkedList.ts
+++ linkedList.ts
@@ -2,6 +2,7 @@ class list {
 
   head: node
   constructor(arraytoSort: number[]) {
+    //temporary work around because TS was throwing a error about this being possibly undefined
     let temp = arraytoSort[0]
     arraytoSort.shift()
 
@@ -10,23 +11,92 @@ class list {
   }
 
   length(count?: number, item?: node) {
+    // this is meant for the first run sets the head to work from
     if (item == undefined) {
       item = this.head
       count = 1
-      console.log("i ran")
     }
+
+    //this is the base case returns the count when it finds tai;
     if (item?.next == undefined) {
       return count
     }
     count++
     return this.length(count, item.next)
   }
+
+  append(value: number) {
+    //intializes the temp to be the head, works down froim there looking for tail
+    let temp = this.head
+    while (temp.next != undefined) {
+      temp = temp.next
+    }
+    temp.next = new node(value)
+  }
+
+  prepend(value: number) {
+    // makes a temporary new variable to store the current head and then makes a new node with its value
+    let temp = new node(value, this.head)
+    this.head = temp
+  }
+
+  tail() {
+    //moves down the list and returns the last item
+    let temp = this.head
+    while (temp.next != undefined) {
+      temp = temp.next
+    }
+    return temp
+  }
+
+
+  at(index: number) {
+    //takes a number and retunrs the value at the index
+    let temp = this.head
+    let count = 0
+    if (index > this.length()) {
+      return null
+    }
+    while (count != index && temp.next != undefined) {
+      temp = temp.next
+      count++
+    }
+    return temp
+  }
+  pop() {
+    //removes the last element and returns its value
+    let temp = this.indexof(this.tail().data)
+    if (temp == undefined) {
+      return null
+    }
+    let index = temp - 1
+    let item = this.at(index)
+    let value = item?.next?.data
+    item.next = undefined
+    return value
+
+  }
+
+  indexof(item: number) {
+    //takes a item and returns its index
+    let temp = this.head
+    let count = 0
+    while (item != temp.data) {
+      if (temp.next == undefined) {
+        return null
+      }
+      temp = temp.next
+      count++
+    }
+    return count
+  }
 }
 
 class node {
+  // a node has its value, and then the value that it links to
   data: number
-  next: node
-  constructor(data: number, next: node) {
+  next?: node
+  constructor(data: number, next?: node) {
     this.data = data
     this.next = next
   }
@@ -34,6 +104,7 @@ class node {
 
 
 const cat = function(array: number[]) {
+  // this works through the array returnign items as nodes
   if (array.length == 0) {
     return
   }
@@ -42,6 +113,17 @@ const cat = function(array: number[]) {
   return new node(value, cat(array))
 }
 
-const test = new list([1, 3, 4])
-console.log(test.head)
-console.log(test.length())
+const test = new list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+console.log(test.tail())
+console.log(test.pop())
+console.log(test.tail())
+
+
+
+
+
+
+
+
+
+
